bfcd1a1d223e68c26d869739b5061cf7224bdf97
Shalabh Raj <shalabh.raj@gmail.com>
2020-05-28 17:56:33 -0700

    Added new endpoint to daytrader-webapp related to expose spring sockJS vul. CVE-2018-1270

---
+++ daytrader-webapp/daytrader-web/src/main/java/org/apache/geronimo/daytrader/javaee6/web/vul/Greeting.java
@@ -0,0 +1,18 @@
+package org.apache.geronimo.daytrader.javaee6.web.vul;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
---
+++ daytrader-webapp/daytrader-web/src/main/java/org/apache/geronimo/daytrader/javaee6/web/vul/HelloMessage.java
@@ -0,0 +1,21 @@
+package org.apache.geronimo.daytrader.javaee6.web.vul;
+
+public class HelloMessage {
+
+    private String name;
+
+    public HelloMessage() {
+    }
+
+    public HelloMessage(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
